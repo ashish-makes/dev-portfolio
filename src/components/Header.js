25a25ceb2f818dee99c87f48212552cae8e241e9
ashish-makes <ashish.makes@gmail.com>
2024-01-26 12:16:13 -0500

    changes made to mobile menu logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -41,7 +41,7 @@ export default function Header() {
             <div className={`mob-nav-menu ${showMobileMenu ? 'show' : ''}`}>
                 <div className='mob-logo-close-btn'>
                     <div>
-                        <a href='/'><img src={logo} alt='logo' /></a>
+                        <Link to='/'><img src={logo} alt='logo' /></Link>
                     </div>
                     <a style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} href='/' className='btn'>
                         {/* <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
